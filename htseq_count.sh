b59a1f2adaa8132acb6c790d48a2f01fe5f6ff6a
yajumo93 <yajumo93@gmail.com>
2021-04-15 14:29:10 +0900

    RNASEQ-pipeline dev

---
+++ htseq_count.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+
+if [ $# -lt 5 ]
+then
+    echo usage: $0 [bamFile] [gtfFile] [output_dir]
+    exit 1
+fi
+
+bam_file=$1
+gtf_file=$2
+output_path=$3
+
+ 
+htseq-count\
+    -i gene_name \
+    -s no \
+    -f bam \
+    $bam_file $gtf_file > $output_path
+
+sleep 10s
